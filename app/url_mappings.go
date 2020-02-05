0c2a41ae7606aab1bb679f02a5d90bcc2d70f4a2
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-02-05 22:48:52 +0100

    added the skeleton of the bookstore-users-api.

---
+++ app/url_mappings.go
@@ -0,0 +1,14 @@
+package app
+
+import (
+  "github.com/vSivarajah/bookstore_users-api/controllers/ping"
+  "github.com/vSivarajah/bookstore_users-api/controllers/users"
+
+)
+
+func MapUrls(){
+  router.GET("/ping", ping.Ping)
+
+  router.POST("/users", users.CreateUser)
+  router.GET("/users/:user_id", users.GetUser)
+}
---
+++ controllers/users/users_controller.go
@@ -0,0 +1,15 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me")
+}
+
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me")
+}
